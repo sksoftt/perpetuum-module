d934b0096b737bd7281c6cce408108562de5be9b
Slava <slava@softtailors.com>
2014-05-28 14:16:04 +0300

    first commit for "all" folder. The commit contains all module till trail module.  The trail module is unfixed. Configure form does not appears.

---
+++ modules/Custom/04_trails/readme.txt
@@ -0,0 +1,1 @@
+This lesson about hooks.
---
+++ modules/Custom/04_trails/steps/1_step.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * 
+ * This file with many hook - to understand how it works
+ * 
+ */
+
+// implements hook_init().
+function trails_init()
+{
+    return;
+}
